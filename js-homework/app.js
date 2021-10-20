1894ebb146ede4609b188f91a17e9130e1a45dd4
vasif-student <vasifah@code.edu.az>
2021-10-20 14:53:45 +0400

    js

---
+++ js-homework/app.js
@@ -0,0 +1,45 @@
+// *** Rectangle ***
+
+
+// function findRectArea(a, b) {
+//     console.log(a * b);
+// }
+
+
+// *********************************
+
+
+
+// ***Alphabetical order***
+
+
+// function makeAlphaOrder(str) {
+//     var arr = str.split('');
+//     var alpha = arr.sort();
+//     return alpha.join('');
+// }
+
+// console.log(makeAlphaOrder("webmaster"));
+
+
+// *********************************
+
+
+// ***Reversing number***
+
+
+// function reverseNum(num) {
+//     let remainder = 0;
+
+//     let result = 0;
+
+//     while (num > 0) {
+//         remainder = num % 10;
+//         result = result * 10 + remainder;
+//         num = parseInt(num / 10);
+//     }
+
+//     console.log(result);
+// }
+
+// reverseNum(4691);
